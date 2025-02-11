44be93a3fb86da4d1d150e90fc57ea5e1df4d6c7
dbigman <dbigman@gascoindustrial.com>
2025-02-10 22:56:02 -0400

    update: added google maps integration as maps_app.py also has index.html

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ data = pd.DataFrame({
     'longitude': [-66.5901, -66.1057, -66.6139]
 })
 
-# Highlight El Morro (San Juan)
+# Highlight El Morro
 highlight = pd.DataFrame({
     'latitude': [18.4706],  # El Morro
-    'longitude': [-66.1239]
+    'longitude': [-66.1239],
+    'label': ["El Morro"]  
 })
 
 # Define the map layers
@@ -21,7 +22,7 @@ puerto_rico_layer = pdk.Layer(
     "ScatterplotLayer",
     data,
     get_position=["longitude", "latitude"],
-    get_radius=2000,  # Adjust the circle size
+    get_radius=250,  # Adjust the circle size
     get_color=[0, 0, 255, 180],  # Blue color (RGBA)
     pickable=True,
 )
@@ -30,16 +31,16 @@ el_morro_layer = pdk.Layer(
     "ScatterplotLayer",
     highlight,
     get_position=["longitude", "latitude"],
-    get_radius=4000,  # Bigger highlight for visibility
+    get_radius=250,  # Bigger highlight for visibility
     get_color=[255, 0, 0, 200],  # Red color (RGBA)
     pickable=True,
 )
 
-# Define the view settings (Zoom into Puerto Rico)
+# Define the view settings 
 view_state = pdk.ViewState(
     latitude=18.45,  # Center the map near San Juan
-    longitude=-66.12,
-    zoom=10,  # Zoom into PR
+    longitude=-66.20,
+    zoom=7,  # Zoom into PR
     pitch=0,
 )
 
